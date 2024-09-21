e7eb905bd41093b21bd97a0b706b2667f5a3a05c
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2024-09-21 23:20:45 +0200

    Dato

--- app/dentistry/page.tsx
+++ app/dentistry/page.tsx
@@ -34,10 +34,10 @@ export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Typography variant="h1" gutterBottom>
+        <Typography variant="h2" gutterBottom>
           DENTISTRY
         </Typography>
-
+        <Typography variant="h6">11.12.23</Typography>
         {questions.map((questionItem: Question, index: number) => (
           <FormControl
             component="fieldset"
